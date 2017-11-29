d8493944f1e4eab4b980daceb0d333176ec2f07a
onurerten1 <onurerten@gmail.com>
2017-11-29 16:54:48 +0300

    Initial commit

---
+++ aldag_graph2/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace aldag_graph2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AE100 27C",
+            "AE200 27C",
+            "AE200 27C (SULU)",
+            "AE300 27C",
+            "AE350 27C",
+            "AE400 27C (TEK FAN)",
+            "AE400 27C (ÇİFT FAN)",
+            "AE700 27C",
+            "AE900 27C"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(265, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(748, 498);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 556);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Fancoil Eğrileri";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ aldag_graph2/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aldag_graph2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)    
+            {
+                case 0:
+                    {
+                        pictureBox1.Image = Properties.Resources.AE100_27C;
+                        break;
+                    }
+                case 1:
+                    {
+                        pictureBox1.Image = Properties.Resources.AE200_27C;
+                        break;
+                    }
+                case 2:
+                    {
+                        pictureBox1.Image = Properties.Resources.AE200_27C__SULU_;
+                        break;
+                    }
+                case 3:
+                    {
+                        pictureBox1.Image = Properties.Resources.AE300_27C;
+                        break;
+                    }
+                case 4:
+                    {
+                        pictureBox1.Image = Properties.Resources.AE350_27C;
+                        break;
+                    }
+                case 5:
+                    {
+                        pictureBox1.Image = Properties.Resources.AE400_27C__TEK_FAN_;
+                        break;
+                    }
+                case 6:
+                    {
+                        pictureBox1.Image = Properties.Resources.AE400_27C__ÇİFT_FAN_;
+                        break;
+                    }
+                case 7:
+                    {
+                        pictureBox1.Image = Properties.Resources.AE700_27C;
+                        break;
+                    }
+                case 8:
+                    {
+                        pictureBox1.Image = Properties.Resources.AE900_27C;
+                        break;
+                    }
+            }
+        }
+    }
+}
